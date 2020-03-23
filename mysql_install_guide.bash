161bbcdef8992e01d3ae6401928be2c1121d550d
huyrom <59599493+huyrom@users.noreply.github.com>
2020-03-23 11:51:02 +0700

    Update mysql_install_guide.bash

--- mysql_install_guide.bash
+++ mysql_install_guide.bash
@@ -1,3 +1,6 @@
+### Quick Start
+### https://docs.microsoft.com/en-us/azure/mysql/quickstart-create-mysql-server-database-using-azure-portal
+
 ### Install mysql client
 ### Reference link: https://www.linode.com/docs/databases/mysql/how-to-install-mysql-on-centos-7/
 
@@ -10,6 +13,9 @@ yum update
 ### The standard tool for interacting with MySQL is the mysql client which installs with the mysql-server package. 
 ### The MySQL client is used through a terminal.
 sudo yum install mysql-server
+
+### Download SSL ###
+### https://docs.microsoft.com/en-us/azure/mysql/howto-configure-ssl ###
 
 ### Connect to MySQL by using the mysql client
 mysql --host vingroupdb.mysql.database.azure.com --user huylq15@vingroupdb  -p --ssl-mode=REQUIRED --ssl-ca=BaltimoreCyberTrustRoot.crt.pem
