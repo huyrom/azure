00593b1f2df1ef9d8aed7d9ec39d14d414db0f31
huyrom <59599493+huyrom@users.noreply.github.com>
2020-03-23 11:40:56 +0700

    Create mysql_install_guide.bash

---
+++ mysql_install_guide.bash
@@ -0,0 +1,15 @@
+### Install mysql client
+### Reference link: https://www.linode.com/docs/databases/mysql/how-to-install-mysql-on-centos-7/
+
+### Download and add the repository, then update.
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum update
+
+### Install MySQL as usual and start the service. 
+### The standard tool for interacting with MySQL is the mysql client which installs with the mysql-server package. 
+### The MySQL client is used through a terminal.
+sudo yum install mysql-server
+
+### Connect to MySQL by using the mysql client
+mysql --host vingroupdb.mysql.database.azure.com --user huylq15@vingroupdb  -p --ssl-mode=REQUIRED --ssl-ca=BaltimoreCyberTrustRoot.crt.pem
